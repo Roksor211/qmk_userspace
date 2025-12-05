ce444e19bdee9a46729743352eccfb5c52b2e8d8
Mischa Lieser <mischa.lieser@gmail.com>
2025-12-05 08:39:30 +0100

    initial

--- keyboards/bastardkb/charybdis/4x6/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/vendor/keymap.c
@@ -25,6 +25,8 @@ enum charybdis_keymap_layers {
     LAYER_LOWER,
     LAYER_RAISE,
     LAYER_POINTER,
+    LAYER_FUNCTION,
+    LAYER_SYMBOLS,
 };
 
 /** \brief Automatically enable sniping-mode on the pointer layer. */
@@ -46,6 +48,8 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define RAISE MO(LAYER_RAISE)
 #define PT_Z LT(LAYER_POINTER, KC_Z)
 #define PT_SLSH LT(LAYER_POINTER, KC_SLSH)
+#define FN_TAB LT(LAYER_FUNCTION, KC_TAB)
+#define SYM_ENT LT(LAYER_SYMBOLS, KC_ENT)
 
 #ifndef POINTING_DEVICE_ENABLE
 #    define DRGSCRL KC_NO
@@ -60,13 +64,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
         KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
+        FN_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LCTL,    PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH, KC_LALT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                   KC_LGUI, KC_SPC,   LOWER,      RAISE,  KC_ENT,
+                                   KC_LGUI, KC_SPC,   LOWER,      RAISE, SYM_ENT,
                                            KC_LALT, KC_BSPC,     KC_DEL
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
@@ -114,6 +118,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_BTN2, KC_BTN1, KC_BTN3,    KC_BTN3, KC_BTN1,
                                            XXXXXXX, KC_BTN2,    KC_BTN2
   //                            ╰───────────────────────────╯ ╰──────────────────╯
+  ),
+
+  [LAYER_FUNCTION] = LAYOUT(
+  // Function keys, system controls and boot helpers.
+        KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,       KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,
+       KC_PSCR,  KC_SCRL,  KC_PAUS,  XXXXXXX,  XXXXXXX,  XXXXXXX,      XXXXXXX,  KC_HOME,    KC_UP,   KC_END,  KC_PGUP,  KC_VOLU,
+       KC_CAPS,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX,      XXXXXXX,  KC_RSFT,  KC_RCTL,  KC_RALT,  KC_RGUI,  KC_VOLD,
+        QK_BOOT,   EE_CLR,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_MUTE,
+                                   _______,  _______,  _______,       _______,  _______,
+                                           _______,  _______,        _______
+  ),
+
+  [LAYER_SYMBOLS] = LAYOUT(
+  // Common punctuation and shifted symbols.
+        KC_TILD,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR, KC_PERC,      KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_PIPE,
+        KC_GRV,   KC_LCBR,  KC_RCBR,  KC_LBRC,  KC_RBRC, KC_LABK,      KC_RABK,  KC_PLUS,   KC_EQL,  KC_UNDS,  KC_MINS,  KC_BSLS,
+        KC_COLN,  KC_SCLN,  KC_QUOT, S(KC_QUOT),KC_COMM,  KC_DOT,      KC_SLSH, S(KC_SLSH), KC_LPRN,  KC_RPRN,  KC_UNDS,  KC_PLUS,
+        _______,  _______,  _______,  _______,  _______,  _______,      _______,  _______,  _______,  _______,  _______,  _______,
+                                   _______,  _______,  _______,       _______,  _______,
+                                           _______,  _______,        _______
   ),
 };
 // clang-format on
